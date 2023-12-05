3c33d7f3a9a8d8cbcd2c96ea1fbb59433a9275c8
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2023-12-05 15:14:31 +0100

    Add variable backgrounds for PageLayout component
    Added backgrounds, improved props documentation, updated variable name.

--- src/pages/PageLayout.tsx
+++ src/pages/PageLayout.tsx
@@ -1,10 +1,20 @@
-import { Grid, Flex, Box } from "@threshold-network/components"
+import {
+  Grid,
+  Flex,
+  Box,
+  SystemStyleObject,
+} from "@threshold-network/components"
 import { ReactNode } from "react"
+import PRIMARY_BACKGROUND_PATH from "../static/images/layout-background-primary.svg"
+import SECONDARY_BACKGROUND_PATH from "../static/images/layout-background-secondary.svg"
 
-const HORIZONTAL_PADDING = 224
+const HORIZONTAL_PADDING = "13.625rem" // 218px
 const BORDER = "1px solid #1A1A22"
 
 //TODO: Remove stale layout components eg. `./Bridge/BridgeLayout.tsx` etc...
+
+type VariantType = "primary" | "secondary"
+type RenderSlotType = Nullable<ReactNode>
 
 interface PageLayoutProps {
   /**
@@ -12,26 +22,41 @@ interface PageLayoutProps {
    *
    * @default undefined
    */
-  renderTop?: Nullable<ReactNode>
+  renderTop?: RenderSlotType
   /**
    * Render slot for content aligned on the left. If the value is `null` the
-   * area is left empty. If the value is the main content occupies
-   * the area.
+   * area is left empty. If the value is not provided the main content (children
+   * occupies the area.
    *
    * @default undefined
    */
-  renderLeft?: Nullable<ReactNode>
+  renderLeft?: RenderSlotType
   /**
    * Render slot for content aligned on the right. If the value is `null` the
-   * area is left empty. If the value is not provided the  main content occupies
-   * the area.
+   * area is left empty. If the value is not provided the main content (children)
+   * occupies the area.
    *
    * @default undefined
    */
-  renderRight?: Nullable<ReactNode>
+  renderRight?: RenderSlotType
+  /**
+   * The variant of the background.
+   *
+   * @default "primary"
+   *  */
+  backgroundVariant?: VariantType
   /** The main content of the page. */
   children: ReactNode
 }
+
+const getBackgroundStyles = (variant: VariantType): SystemStyleObject => ({
+  bgImage:
+    variant === "primary" ? PRIMARY_BACKGROUND_PATH : SECONDARY_BACKGROUND_PATH,
+  bgRepeat: "no-repeat",
+  bgSize: variant === "primary" ? "contain" : "cover",
+  bgPosition: "bottom",
+  bgAttachment: "fixed",
+})
 
 /**
  * Renders the page wrapped in layout with render slots for content alignment.
@@ -39,9 +64,16 @@ interface PageLayoutProps {
  * @return {JSX.Element} The rendered page wrapped in layout.
  */
 export default function PageLayout(props: PageLayoutProps) {
-  const { renderTop, renderLeft, renderRight, children, ...restProps } = props
+  const {
+    renderTop,
+    renderLeft,
+    renderRight,
+    backgroundVariant = "secondary",
+    children,
+    ...restProps
+  } = props
 
-  const outletContainerColumnSpan = [
+  const childrenContainerColumnSpan = [
     renderLeft === null || renderLeft !== undefined ? 2 : 1,
     renderRight === null || renderRight !== undefined ? 4 : 5,
   ].join("/")
@@ -52,6 +84,7 @@ export default function PageLayout(props: PageLayoutProps) {
       alignItems={"normal"}
       bg={"black"}
       minHeight={"100vh"}
+      sx={getBackgroundStyles(backgroundVariant)}
       {...restProps}
     >
       {/* <Header /> */}
@@ -62,14 +95,14 @@ export default function PageLayout(props: PageLayoutProps) {
       )}
       <Grid
         templateColumns={"repeat(4, .5fr)"}
-        w={`calc(100% - ${HORIZONTAL_PADDING * 2}px)`}
+        w={`calc(100% - calc(${HORIZONTAL_PADDING} * 2))`}
         margin={"auto"}
         borderLeft={renderLeft !== undefined ? BORDER : ""}
         borderRight={renderRight !== undefined ? BORDER : ""}
         flex={1}
       >
         {!!renderLeft && <Box p={10}>{renderLeft}</Box>}
-        <Box gridColumn={outletContainerColumnSpan} borderX={BORDER}>
+        <Box gridColumn={childrenContainerColumnSpan} borderX={BORDER}>
           {children}
         </Box>
         {!!renderRight && <Box p={10}>{renderRight}</Box>}
