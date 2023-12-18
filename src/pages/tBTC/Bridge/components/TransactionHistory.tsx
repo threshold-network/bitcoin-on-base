43f61a8cd6df856a21ffab3dfbd3a9222865b8e2
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2023-12-18 17:02:48 +0100

    Restyle `TransactionHistory` component
    Added documentation

---
+++ src/pages/tBTC/Bridge/components/TransactionHistory.tsx
@@ -0,0 +1,58 @@
+import {
+  Box,
+  BodyMd,
+  List,
+  ListItem,
+  Link,
+  HStack,
+} from "@threshold-network/components"
+import { FC } from "react"
+import { FaChevronRight as ChevronIcon } from "react-icons/fa"
+import {
+  Chain,
+  createLinkToBlockExplorerForChain,
+} from "../../../../components/ViewInBlockExplorer"
+import { ExplorerDataType } from "../../../../utils/createEtherscanLink"
+
+export type TransactionHistoryItemType = {
+  label: string
+  txHash?: string
+  chain: Chain
+}
+
+interface Props {
+  /**
+   * Items to display in the transaction history list as block explorer links.
+   */
+  items: TransactionHistoryItemType[]
+}
+
+/**
+ * Displays a list of transactions as block explorer links.
+ * @return {ReactNode}
+ */
+export const TransactionHistory: FC<Props> = ({ items, ...restProps }) => (
+  <Box {...restProps}>
+    <BodyMd fontWeight="medium" mb="4">
+      Transaction History
+    </BodyMd>
+    <List spacing="4">
+      {items
+        .filter((item) => !!item.txHash)
+        .map(({ txHash, label, chain = "ethereum" }) => {
+          const link = createLinkToBlockExplorerForChain[chain](
+            txHash!,
+            ExplorerDataType.TRANSACTION
+          )
+          return (
+            <ListItem key={txHash}>
+              <HStack as={Link} spacing="2.5" isExternal href={link}>
+                <ChevronIcon color="#66F9FF" size={16} />
+                <BodyMd>{label}.</BodyMd>
+              </HStack>
+            </ListItem>
+          )
+        })}
+    </List>
+  </Box>
+)
