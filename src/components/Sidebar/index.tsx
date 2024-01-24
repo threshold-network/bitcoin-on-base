470d1974e5232584eeac3eda087b54409e28312b
michalsmiarowski <michal.smiarowski@thesis.co>
2024-01-24 16:30:43 +0100

    Remove `useMemo` from Sidebar.
    `useMemo` hook is unnecessary now since creating `navItems` variable is not a
    costly operation anyway.

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,25 +1,18 @@
 import { NavItemDetail } from "./NavItem"
-import { useLocation } from "react-router-dom"
-import { useMemo } from "react"
 import DesktopSidebar from "./DesktopSidebar"
 import MobileSidebar from "./MobileSidebar"
 import { tBTCFill } from "../../static/icons/tBTCFill"
 import { tBTCOutline } from "../../static/icons/tBTCOutline"
 
 const Sidebar = () => {
-  const { pathname } = useLocation()
-  const navItems: NavItemDetail[] = useMemo(() => {
-    const navItems: NavItemDetail[] = [
-      {
-        text: "tBTC",
-        activeIcon: tBTCFill,
-        passiveIcon: tBTCOutline,
-        href: "/tBTC/how-it-works",
-      },
-    ]
-
-    return navItems
-  }, [pathname])
+  const navItems: NavItemDetail[] = [
+    {
+      text: "tBTC",
+      activeIcon: tBTCFill,
+      passiveIcon: tBTCOutline,
+      href: "/tBTC/how-it-works",
+    },
+  ]
 
   return (
     <>
