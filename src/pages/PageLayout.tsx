9a864bd2cdfc53d6095a07f97f6bba0fff5d2177
Kamil Pyszkowski <kamil.pyszkowski@akena.co>
2024-01-25 14:35:02 +0100

    Add resposive styles to `PageLayout` component

--- src/pages/PageLayout.tsx
+++ src/pages/PageLayout.tsx
@@ -4,12 +4,13 @@ import {
   FlexProps,
   Grid,
   SystemStyleObject,
+  useMediaQuery,
 } from "@threshold-network/components"
 import { FC, ReactNode } from "react"
 import PRIMARY_BACKGROUND_PATH from "../static/images/layout-background-primary.svg"
 import SECONDARY_BACKGROUND_PATH from "../static/images/layout-background-secondary.svg"
 
-const HORIZONTAL_PADDING = "13.625rem" // 218px
+const CONTENT_MAX_WIDTH = "89.25rem" // 1428px
 const BORDER = "1px solid hsla(0, 0%, 100%, 10%)"
 
 //TODO: Remove stale layout components eg. `./Bridge/BridgeLayout.tsx` etc...
@@ -78,6 +79,10 @@ const PageLayout: FC<PageLayoutProps> = (props) => {
     ...restProps
   } = props
 
+  const [hasContentMaxWidth] = useMediaQuery(
+    `(min-width: ${CONTENT_MAX_WIDTH})`
+  )
+
   const childrenContainerColumnSpan = [
     renderLeft === null || renderLeft !== undefined ? 2 : 1,
     renderRight === null || renderRight !== undefined ? 4 : 5,
@@ -99,23 +104,34 @@ const PageLayout: FC<PageLayoutProps> = (props) => {
     >
       {/* <Header /> */}
       {!!renderTop && (
-        <Box px={HORIZONTAL_PADDING} py={6} borderBottom={BORDER}>
-          <FixedContainer>{renderTop}</FixedContainer>
+        <Box px={hasContentMaxWidth ? 0 : 6} py={6} borderBottom={BORDER}>
+          <FixedContainer mx="auto" w="full" maxW={CONTENT_MAX_WIDTH}>
+            {renderTop}
+          </FixedContainer>
         </Box>
       )}
       <FixedContainer flex={1} w={"full"}>
         <Grid
-          templateColumns={"repeat(4, .5fr)"}
-          w={`calc(100% - calc(${HORIZONTAL_PADDING} * 2))`}
-          margin={"auto"}
+          templateColumns={{ base: "auto", lg: "repeat(4, .5fr)" }}
+          w="full"
+          maxW={CONTENT_MAX_WIDTH}
+          mx="auto"
           {...borderX}
           flex={1}
         >
-          {!!renderLeft && <Box p={10}>{renderLeft}</Box>}
-          <Box gridColumn={childrenContainerColumnSpan} {...borderX}>
+          {!!renderLeft && (
+            <Box p={hasContentMaxWidth ? 10 : 6}>{renderLeft}</Box>
+          )}
+          <Box
+            order={{ base: -1, lg: "unset" }}
+            gridColumn={{ base: "auto", lg: childrenContainerColumnSpan }}
+            {...borderX}
+          >
             {children}
           </Box>
-          {!!renderRight && <Box p={10}>{renderRight}</Box>}
+          {!!renderRight && (
+            <Box p={hasContentMaxWidth ? 10 : 6}>{renderRight}</Box>
+          )}
         </Grid>
       </FixedContainer>
     </Flex>
